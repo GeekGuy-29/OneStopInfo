38fd7ab76c93ca08ca9e4fda85a50126b63d8f58
Chiranjeev Veer Srivastava <chiranjeev.important@gmail.com>
2022-10-05 21:40:02 +0530

    Added Script to Parse through the .json files

---
+++ script.js
@@ -0,0 +1,20 @@
+fetch("jobs.json")
+  .then(function (response) {
+    return jobs.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+        for (var i = 0; i < data.length; i++) {
+            var div = document.createElement("div");
+            div.innerHTML = 'Name: ' + data[i].JobRole + ' ' + data[i].JobDescription;
+            mainContainer.appendChild(div);
+    }
+}
+
+
