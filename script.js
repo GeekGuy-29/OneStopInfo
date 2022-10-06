4e720bbeed1a7dcb2129367a532d111466925b10
A-Spiral-Forge <64008676+ABHAY0PARIHAR@users.noreply.github.com>
2022-10-06 15:16:20 +0530

    Script added to read json and output in console and render as HTML

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-fetch("http://192.168.29.27:8767")
-  .then(function (response) {
-    return jobs.json();
-  })
-  .then(function (data) {
-    appendData(data);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
-function appendData(data) {
-    var mainContainer = document.getElementById("myData");
-        for (var i = 0; i < data.length; i++) {
-            var div = document.createElement("div");
-            div.innerHTML = 'Name: ' + data[i].JobRole + ' ' + data[i].JobDescription;
-            mainContainer.appendChild(div);
-    }
-}
+const mainElement = document.querySelector('#myData');
 
+const renderData = (data, fileName) => {
+	let htmlString = `<h1 class="data--heading">${
+		fileName[0].toUpperCase() + fileName.slice(1)
+	}</h1>`;
 
+	data.forEach((item) => {
+		const [key1, key2, key3] = Object.keys(item);
+		htmlString += `<div class="data--item" id="${fileName}-${key1}">
+            <h2 class="data--item__name">${item[key2]}</h2>
+            <p class="data--item__description">${item[key3]}</p>
+        </div>`;
+	});
+};
+
+const loadData = async (fileName) => {
+	const url = window.location.origin + '/' + fileName + '.json';
+	const response = await fetch(url);
+	const data = await response.json();
+	console.log(data);
+	renderData(data, fileName);
+};
+
+const init = async () => {
+	const fileNamesUrl = window.location.origin + '/infofilenames.json';
+	const response = await fetch(fileNamesUrl);
+	const fileNames = await response.json();
+	console.log(fileNames);
+	fileNames.forEach(async (fileName) => {
+		await loadData(fileName);
+	});
+};
+
+init();
